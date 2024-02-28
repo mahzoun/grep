b06f873221a2062f09bf299338d589746cf7b6cf
mairon <mairon.mahzoun@gmail.com>
2024-02-28 17:25:14 +0100

    add reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env,fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +8,6 @@ fn main() {
     println!("Program name is {}", program_name);
     println!("Query is {}", query);
     println!("File path is {}", file_path);
+    let content = fs::read_to_string(file_path).expect("Error in opening the file");
+    println!("with text:\n{content}");
 }
