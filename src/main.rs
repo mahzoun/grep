9cbade5d8cb955c6a1745d816f418e154a10caa9
mairon <mairon.mahzoun@gmail.com>
2024-07-11 16:08:29 +0200

    add case sensitive env

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use grep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsin arguments: {err}");
+        eprintln!("Problem parsin arguments: {err}");
         process::exit(1);
     });
     if let Err(e) = grep::run(config){
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
