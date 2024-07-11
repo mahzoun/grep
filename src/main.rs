47da1ae0dc48f2c77c3dbfa55a0b0eac703a6fc3
mairon <mairon.mahzoun@gmail.com>
2024-07-11 12:12:49 +0200

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::{env,fs};
+use std::{env, process};
+use grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    let program_name = &args[0];
-    println!("Program name is {}", program_name);
-    println!("Query is {}", query);
-    println!("File path is {}", file_path);
-    let content = fs::read_to_string(file_path).expect("Error in opening the file");
-    println!("with text:\n{content}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsin arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = grep::run(config){
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
