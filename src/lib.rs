15e7d2fd1e7f302caab9ebebf94f53f24775da7b
mairon <mairon.mahzoun@gmail.com>
2024-07-11 15:49:11 +0200

    add grep functionality

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,21 @@ use std::fs;
 use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    println!("Query is {}", config.query);
-    println!("File path is {}", config.file_path);
     let content = fs::read_to_string(config.file_path)?;    
-    println!("with text:\n{content}");
+    for line in search(&config.query, &content){
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 pub struct Config {
@@ -23,4 +33,19 @@ impl Config {
         let file_path = args[2].clone();
         Ok(Config{query, file_path})
     } 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
